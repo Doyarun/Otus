4ebb44aeb8fedfc459d6580ca850e8271aeea7ac
Doyarun <rszaytsev@gmail.com>
2024-11-06 19:40:58 +0300

    output update
    Fix output text
    Fix executable name

--- Lesson1/src/main.cpp
+++ Lesson1/src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Version " << version() << std::endl;
+    std::cout << "Version: " << version() << std::endl;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
